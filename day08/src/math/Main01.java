d718e745ed560fbd8bbd930514853036f581ef14
hansamk97 <134520462+hansamk97@users.noreply.github.com>
2023-08-18 18:15:12 +0900

    Add files via upload

---
+++ day08/src/math/Main01.java
@@ -0,0 +1,44 @@
+package math;
+
+public class Main01 {
+
+	public static void main(String[] args) {
+		// 주어진 두 수에 대한 최대값 구하기
+		int max = Math.max(100, 2105);
+		System.out.println("최대값 : " + max);
+		
+		// 주어진 두 수에 대한 최소값 구하기
+		int min = Math.min(100, 22210);
+		System.out.println("최소값 : " + min);
+		
+		// 주어진 두 수에 대한 절대값 구하기
+		int abs = Math.abs(-10022210);
+		System.out.println("절대값 : " + abs);
+		
+		// 소수점 반올림
+		long num = Math.round(3.12345);
+		System.out.println("소수점 반올림 : " + num);
+		
+		// 원주율
+		System.out.println("원주율 : " + Math.PI);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
